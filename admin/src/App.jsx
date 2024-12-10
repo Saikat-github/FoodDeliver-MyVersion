435882e686c115f177ec6cd06ff87cb25d614433
Saikat-github <145221042+Saikat-github@users.noreply.github.com>
2024-12-10 12:12:33 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Edit from './pages/edit/Edit'
 
 const App = () => {
-    const url = 'http://localhost:4000'
+    const url = 'https://fooddeliver-myversion-backend.onrender.com'
 
   return (
     <div className='font-Outfit'>
