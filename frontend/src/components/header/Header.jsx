427d058cf3e6269b74df9bec104e322f9c67f9fa
Saikat <saikatsaha.rph@gmail.com>
2024-12-10 12:38:18 +0530

    modified hero image path

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Header = () => {
   return (
-    <div className='h-[40vw] mb-10 sm:mt-10 mt-0 mx-auto bg-[url("./heroimg5.jpg")] bg-cover relative bg-no-repeat rounded-lg' >
+    <div className='h-[40vw] mb-10 sm:mt-10 mt-0 mx-auto bg-[url("../..assets/heroimg5.jpg")] bg-cover relative bg-no-repeat rounded-lg' >
         <div className='relative flex flex-col items-start  gap-[1.5vw] max-w-[50%] lg:top-52 lg:left-16 animate-[fadeIn_3s] px-4 py-4'>
             <h2 className='lg:text-5xl sm:text-3xl font-semibold text-white drop-shadow-md'>Order your favourite food here</h2>
             <p className='text-white hidden lg:inline drop-shadow-md'>Choose from a diverse menu featuring a delecable array of dishes crafted with the finest ingredient and culinary expertise.</p>
