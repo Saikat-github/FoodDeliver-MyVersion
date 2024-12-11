f7a9d92f494b6661be26b38691dd80f9c3d984bf
Saikat <saikatsaha.rph@gmail.com>
2024-12-11 07:25:10 +0530

    added frontend url in backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placing user oder for frontend
 const placeOrder = async (req, res) => {
-    const url = "http://localhost:5173"
+    const url = "https://fooddeliver-myversion-frontend.onrender.com/"
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
