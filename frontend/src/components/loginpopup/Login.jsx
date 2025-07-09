e59414dd7eed69ba9c4f50d42f90df02a593293d
Saikat <saikatsaha.rph@gmail.com>
2025-07-09 08:04:03 +0530

    solved toast import error

--- frontend/src/components/loginpopup/Login.jsx
+++ frontend/src/components/loginpopup/Login.jsx
@@ -5,7 +5,7 @@ import { setShowLogin, setToken } from '../../features/cartSlice';
 import { useForm } from 'react-hook-form';
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
-
+import { toast } from "react-toastify"
 
 
 
